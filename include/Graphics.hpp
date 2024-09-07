0154a0eeebb8b91be8adf1d1a5740bb229ba44e4
Francesco Colla <francescocolla1801@gmail.com>
2024-09-07 15:34:36 +0200

    little update on map, updated segmentation test

--- include/Graphics.hpp
+++ include/Graphics.hpp
@@ -12,7 +12,7 @@ public:
     static std::vector<cv::RotatedRect> getBoxes();
     static cv::Mat drawMap(const std::vector<cv::RotatedRect>& parkingSpaces);
     static void fillRotatedRectsWithCar(cv::Mat& image, const std::vector<cv::RotatedRect>& rectangles, const std::vector<int>& carIndices);
-    static void mapOverlay(cv::Mat& src, cv::Mat map);
+    static void mapOverlay(cv::Mat& src, const cv::Mat& map);
 
 private:
     static void getParkingRow(std::vector<cv::RotatedRect>& parkingSlots , int numParking, float angle, int xOffset, float horizontalOffsetAdjustment, int yOffset, float verticalOffsetAdjustment, int parkingWidth, int parkingHeight, int spacing, int extraSpacing = 0, bool isDoubleRow = false, bool isLowerSet = false);
